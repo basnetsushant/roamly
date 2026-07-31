48bc4b669dd6ec886532a47d34f74851304a51af
Sushant Basnet <sushantbasnet01@gmail.com>
2026-07-31 16:24:59 +0545

    Update build script

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,6 @@
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import PropertyRating from "@/components/card/PropertyRating";
+import Amenities from "@/components/properties/Amenities";
 import BookingCalendar from "@/components/properties/BookingCalendar";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
 import Description from "@/components/properties/Description";
@@ -8,10 +9,18 @@ import PropertyDetails from "@/components/properties/PropertyDetails";
 import ShareButton from "@/components/properties/ShareButton";
 import UserInfo from "@/components/properties/UserInfo";
 import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
 import { fetchPropertiesDetails } from "@/utils/actions";
+import dynamic from "next/dynamic";
 import { redirect } from "next/navigation";
-import { FaShare } from "react-icons/fa";
 
+const DynamicMap = dynamic(
+  () => import("@/components/properties/PropertyMap"),
+  {
+    ssr: !!false,
+    loading: () => <Skeleton className="h-100 w-full" />,
+  },
+);
 async function page({ params }: { params: { id: string } }) {
   const { id } = await params;
   const property = await fetchPropertiesDetails(id);
@@ -53,8 +62,9 @@ async function page({ params }: { params: { id: string } }) {
           <PropertyDetails details={details} />
           <UserInfo profile={{ firstName, profileImage }} />
           <Separator className="mt-4" />
-
           <Description description={property.description} />
+          <Amenities amenities={property.amenities} />
+          <DynamicMap countryCode={property.country} />
         </div>
 
         <div className="lg:col-span-4 flex flex-col items-center">
